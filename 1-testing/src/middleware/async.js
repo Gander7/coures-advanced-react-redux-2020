7d33773d797c7abd79c9a822d14e310a0148e185
Zach <zach@basebridge.com>
2020-01-28 14:37:47 -0400

    1 - replaced reduxPromise with custom async

---
+++ 1-testing/src/middleware/async.js
@@ -0,0 +1,16 @@
+export default ({dispatch}) => (next) => (action) => {
+    console.log(action)
+
+    // Check to see if action has a promise in payload
+    // if it doesn't have a promise, pass it on
+    if (!action.payload || !action.payload.then) {
+        return next(action)
+    }
+
+    // if it does have a promise, wait for it to resolve
+    // create a new action and dispatch it
+    action.payload.then(function(response) {
+        const newAction = { ...action, payload: response }
+        dispatch(newAction)
+    })
+}

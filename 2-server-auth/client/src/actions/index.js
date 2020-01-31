99032482876dc4043131f65263071904a14de2bc
Zach <zach@basebridge.com>
2020-01-31 13:17:38 -0400

    client - hooked up auth and fixed index action

--- 2-server-auth/client/src/actions/index.js
+++ 2-server-auth/client/src/actions/index.js
@@ -4,7 +4,8 @@ import { AUTH_USER, AUTH_ERROR } from './types'
 export const signup = (formProps, callback) => async (dispatch) => {
     try {
         const res = await axios.post('http://localhost:5000/api/Authentication', { username: formProps.email, password: formProps.password })
-        dispatch({ type: AUTH_USER, payload: res.data.token })
+        console.log(res)
+        dispatch({ type: AUTH_USER, payload: res.data.data.token })
         callback()
     } catch (e) {
         // TODO: Log Error
--- 2-server-auth/client/src/components/Feature.js
+++ 2-server-auth/client/src/components/Feature.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import requireAuth from './requireAuth'
 
 class Feature extends React.Component {
     render() {
@@ -10,4 +11,4 @@ class Feature extends React.Component {
     }
 }
 
-export default Feature
+export default requireAuth(Feature)
